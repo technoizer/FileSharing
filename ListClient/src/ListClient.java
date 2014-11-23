270609c180e527b49556de6099ed9f77cca75f03
Muhammad Izzuddin <Muhammad Izzuddin@technoizer-pc>
2014-11-23 13:17:23 +0700

    kelas list client

---
+++ ListClient/src/ListClient.java
@@ -0,0 +1,32 @@
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Muhammad Izzuddin
+ */
+public class ListClient implements Serializable{
+    private ArrayList <String> names;
+
+    /**
+     * @return the names
+     */
+    public ArrayList <String> getNames() {
+        return names;
+    }
+
+    /**
+     * @param names the names to set
+     */
+    public void setNames(ArrayList <String> names) {
+        this.names = names;
+    }
+            
+}
